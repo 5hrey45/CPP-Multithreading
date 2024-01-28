e8a2c52cdca5396c60ad194f37b010d5b0eaa517
Shreyas S S <shreyasnarve1611@gmail.com>
2024-01-28 17:04:31 +0530

    update mutex.cpp

--- src/Mutex.cpp
+++ src/Mutex.cpp
@@ -10,6 +10,7 @@ void print(std::string s) {
 	{
 		std::cout << s[0] << s[1] << s[2] << std::endl;
 	}
+	// never forget to unlock the mutex to free up the resource
 	mtx.unlock();
 }
 
@@ -18,6 +19,7 @@ int main() {
 	std::thread thr2(print, "def");
 	std::thread thr3(print, "xyz");
 
+	// do not forget to join the thread, otherwise results in zombie threads
 	thr1.join();
 	thr2.join();
 	thr3.join();
