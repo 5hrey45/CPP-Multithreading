2d32772cc5f36f7564f220f4e050b9ada6c0a30a
Shreyas S S <shreyasnarve1611@gmail.com>
2024-01-30 12:51:34 +0530

    fix typo in deadlock

--- src/deadlock.cpp
+++ src/deadlock.cpp
@@ -26,7 +26,7 @@ void task2() {
 	std::this_thread::sleep_for(50ms);
 	std::cout << "Thread 2 is trying to lock mutex 1...\n";
 	std::unique_lock ug2(mut1);
-	std::cout << "Thread 2 has locked mutex 2\n";
+	std::cout << "Thread 2 has locked mutex 1\n";
 	std::this_thread::sleep_for(50ms);
 	ug1.unlock();
 	ug2.unlock();
